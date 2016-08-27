af796640424d90c82d84be200b16df59bf4559e4
Héctor Menéndez <etor.mx@gmail.com>
2016-08-27 16:33:34 -0500

    Updated configuration files to latest gik-standard.

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Test = require('feliz.test');
+
+Test([
+    { desc:'The feliz-views plugin', test: function(tape){
+        tape.skip('should have tests defined.');
+        tape.end();
+    }}
+]).subscribe();
